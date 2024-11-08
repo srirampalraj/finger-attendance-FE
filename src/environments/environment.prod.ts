dae4eb6f85bd275c09c70981526ed672ba959120
srirampalraj <srirampalraj@gmail.com>
2024-11-08 16:59:38 +0530

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyCnm-ix0wBjJGgJn6MMiQoFKXEVBMIyGUM',
+  authDomain: 'fingerprint-attendance-98969.firebaseapp.com',
+  projectId: 'fingerprint-attendance-98969',
+  storageBucket: 'fingerprint-attendance-98969.firebasestorage.app',
+  messagingSenderId: '629851221270',
+  appId: '1:629851221270:web:452615c9fb8a7426c7b437',
 };
